ff611f184cd49c817a4ec714b355083fdf581e9d
Ragnar <delesky@me.com>
2023-02-17 22:57:30 +0100

    add viewhelper for the front end to check html string fo embeded videos

--- Classes/ViewHelper/Backend/IsServiceSelectedViewHelper.php
+++ Classes/ViewHelper/Backend/IsServiceSelectedViewHelper.php
@@ -1,20 +1,32 @@
 <?php
 
-
 namespace JD\JdUsercentricsConfigurator\ViewHelper\Backend;
-
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
+/**
+ * Class IsServiceSelectedViewHelper
+ *
+ * This ViewHelper is used to check if a Service is selected in the backend Module.
+ *
+ * @package JD\JdUsercentricsConfigurator\ViewHelper\Backend
+ * @author Johannes Delesky, Developer
+ */
 class IsServiceSelectedViewHelper extends AbstractViewHelper
 {
+    /**
+     * {@inheritDoc}
+     */
     public function initializeArguments()
     {
         $this->registerArgument('selectedServices', 'string', 'The list of selected Services', TRUE);
         $this->registerArgument('crServiceId', 'string', 'The service to check if selected', TRUE);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function render()
     {
         $selectedServiceArray = GeneralUtility::trimExplode(',', $this->arguments['selectedServices']);
